b1eca1c6626186014342373ce81e9382349c5ea3
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-01-06 18:47:54 -0700

    first commit

---
+++ challenge1.js
@@ -0,0 +1,24 @@
+// BMI cacluator
+
+// Test data 1 
+// let markMass = 78;
+// let johnMass = 92;
+
+// let markHeight = 1.69;
+// let johnHeight = 1.95;
+
+// Test data 2 
+let markMass = 95; 
+let johnMass = 85; 
+
+let markHeight = 1.88;
+let johnHeight = 1.76;
+
+
+const bmiMark = markMass / markHeight ** 2;
+const bmiJohn = johnMass / johnHeight ** 2. 
+
+let markHigherBMI = bmiMark > bmiJohn
+
+console.log(bmiMark, bmiJohn);
+console.log(markHigherBMI);

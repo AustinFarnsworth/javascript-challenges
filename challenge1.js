737589efbccbff2aa071162136b203f7b2f8bc1e
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-01-11 20:04:06 -0700

    challenge 3

--- challenge1.js
+++ challenge1.js
@@ -1,32 +1,46 @@
-// BMI cacluator
+// // BMI cacluator
 
-// Test data 1 
-// let markMass = 78;
-// let johnMass = 92;
+// // Test data 1 
+// // let markMass = 78;
+// // let johnMass = 92;
 
-// let markHeight = 1.69;
-// let johnHeight = 1.95;
+// // let markHeight = 1.69;
+// // let johnHeight = 1.95;
 
-// Test data 2 
-let markMass = 95; 
-let johnMass = 85; 
+// // Test data 2 
+// let markMass = 95; 
+// let johnMass = 85; 
 
-let markHeight = 1.88;
-let johnHeight = 1.76;
+// let markHeight = 1.88;
+// let johnHeight = 1.76;
 
 
-const bmiMark = markMass / markHeight ** 2;
-const bmiJohn = johnMass / johnHeight ** 2. 
+// const bmiMark = markMass / markHeight ** 2;
+// const bmiJohn = johnMass / johnHeight ** 2. 
 
-let markHigherBMI = bmiMark > bmiJohn
+// let markHigherBMI = bmiMark > bmiJohn
 
-console.log(bmiMark, bmiJohn);
-console.log(markHigherBMI);
+// console.log(bmiMark, bmiJohn);
+// console.log(markHigherBMI);
 
-// Coding Challenge 2
+// // Coding Challenge 2
 
-if (markHigherBMI === true) {
-    console.log("Marks BMI is higher than Johns");
+// if (markHigherBMI === true) {
+//     console.log("Marks BMI is higher than Johns");
+// } else {
+//     console.log("Johns BMI is higher than Marks");
+// }
+
+// // Challenge 3 
+var dolphinsScore = (96 + 108 + 89) / 3; 
+var koalasScore = (88 + 91 + 120) /3;
+
+if (dolphinsScore >= koalasScore) {
+    console.log("Dolphins Win!");
 } else {
-    console.log("Johns BMI is higher than Marks");
+    console.log("Koalas Win!");
 }
+
+
+
+

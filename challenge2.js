b09eee83028ab5d81033a54f70265ecaca286ba9
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-01-20 19:34:14 -0700

    challenge 3

--- challenge2.js
+++ challenge2.js
@@ -24,18 +24,55 @@
 // Challenge 2
 
 
-const bill = "100";
+// const bill = "100";
 
 
-// const calcTip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
-const calcTip = function (bill) {
-    return bill >= 50 && bill >= 300 ? bill * .15 : bill * .2;
-}
+// // const calcTip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill >= 300 ? bill * .15 : bill * .2;
+// }
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
  
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
+
+// Challenge 3 
+
+const markMiller = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    weight: 78,
+    height: 1.69,
+
+    calcBMI: function() {
+        this.bmi = this.weight / this.height ** 2
+        return this.bmi; 
+    }
+
+};
+// console.log(markMiller.calcBMI());
+
+const johnSmith = {
+    firstName: 'John',
+    lastName: 'Smith',
+    weight: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.weight / this.height ** 2
+        return this.bmi; 
+    }
+
+}
+// console.log(johnSmith.calcBMI());
+
+if (markMiller.calcBMI() > johnSmith.calcBMI()) {
+    console.log(`Marks BMI ${markMiller.calcBMI()} is higher than Johns BMI ${johnSmith.calcBMI()}`);
+} else if (johnSmith.calcBMI() > markMiller.calcBMI()) {
+    console.log(`Johns BMI ${johnSmith.calcBMI()} is higher than Marks BMI ${markMiller.calcBMI()}`);
+} else {
+    console.log('They have the same BMi');
+}
